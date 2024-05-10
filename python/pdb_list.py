b605bed241370bf58926c712c309e3d25bc36ca7
Witold Swierzy <witold@swierzy.eu>
2024-05-10 16:13:03 +0200

    aaa

--- python/pdb_list.py
+++ python/pdb_list.py
@@ -5,8 +5,10 @@ data = json.load(sys.stdin)['data']
 for c in data:
         if c['lifecycle-state']=='TERMINATED':
             print (" db: "+c['pdb-name']+":TERMINATED")
+            print ("")
         elif c['lifecycle-state']=='PROVISIONING':
             print (" db: "+c['pdb-name']+":PROVISIONING")
+            print ("")
         else:
             print (" db: "+c['pdb-name']+":"+c['open-mode']+":"+c['connection-strings']['pdb-default'].split("/",1)[1])
             print (" id: "+c['id'])
